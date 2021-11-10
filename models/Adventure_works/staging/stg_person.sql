8fa63c0d80afd5f45ccecc807ff79a04d88104e4
mviyzeixzddafnoxjw@sdvrecft.com <mviyzeixzddafnoxjw@sdvrecft.com>
2021-11-10 23:59:13 +0000

    Added table Person

---
+++ models/Adventure_works/staging/stg_person.sql
@@ -0,0 +1,19 @@
+with 
+    source as (
+        select
+        
+        -- Primary key
+       {{ dbt_utils.surrogate_key(['businessentityid']) }} as sk_businessentityid
+
+       --Foreign Keys
+
+       --Information
+       , persontype
+       , title
+       , firstname
+       , lastname
+
+    
+    from {{ source('desafio_final','person') }} 
+    )
+    select * from source 
